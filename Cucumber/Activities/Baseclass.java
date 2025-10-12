62ccdb7580edcbc32c8543afe3f36f480b92378c
rakesh284619 <Reniguntla.RakeshRakesh@ibm.com>
2025-10-12 09:56:17 +0530

    Update Baseclass.java

--- Cucumber/Activities/Baseclass.java
+++ Cucumber/Activities/Baseclass.java
@@ -1,5 +1,11 @@
-package Cucumber;
+package stepDefinitions;
 
-public class Baseclass {
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//BaseClass: This class will contain static object for those libraries 
+//that will be used across all step definitions, like driver, wait, etc.
+public class BaseClass {
+    // Create a new instance of the Firefox driver
+    static WebDriver driver;
+    static WebDriverWait wait;
 }
