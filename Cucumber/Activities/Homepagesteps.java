78107a81ae0f7c7a91b70ab094a43f87b79ea6aa
rakesh284619 <Reniguntla.RakeshRakesh@ibm.com>
2025-10-12 09:57:23 +0530

    Update Homepagesteps.java

--- Cucumber/Activities/Homepagesteps.java
+++ Cucumber/Activities/Homepagesteps.java
@@ -1,5 +1,30 @@
-package Cucumber;
+package stepDefinitions;
 
-public class Homepagesteps {
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Homepagesteps extends BaseClass {
+	//Below text in "" is copied from Activity1.feature
+	@Given ("the user is on the TS home page")
+	public void openhomepage() {
+		driver.get("https://training-support.net");
+		Assertions.assertEquals(driver.getTitle(), "Training Support");
+	}
+	
+	@When ("they click on About Us page")
+	public void clickbutton() {
+		driver.findElement(By.linkText("About Us")).click();
+		
+	}
+	
+	@Then ("they are redirected to About US page")
+	public void verifypage() {
+		Assertions.assertEquals(driver.getTitle(), "About Training Support");
+	}
+	
 
 }
