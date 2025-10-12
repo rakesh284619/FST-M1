aef0b2a071cb76b383701a9500f9e18c09c1c612
rakesh284619 <Reniguntla.RakeshRakesh@ibm.com>
2025-10-12 09:56:40 +0530

    Update Fixtures.java

--- Cucumber/Activities/Fixtures.java
+++ Cucumber/Activities/Fixtures.java
@@ -1,5 +1,27 @@
-package Cucumber;
+package stepDefinitions;
 
-public class Fixtures {
+import java.time.Duration;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
+
+//This class will contain the Before and After functions.
+public class Fixtures extends BaseClass {
+	//This method is executed once before all test scenarios.
+    @BeforeAll
+    public static void setUp() {
+        // Initialize Firefox Driver
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    
+    //This method is executed once after all test scenarios.
+    @AfterAll
+    public static void tearDown() {
+        // Close the browser
+        driver.quit();
+    }
 }
