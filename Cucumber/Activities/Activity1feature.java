eae7ca18b5ef0292b2f5613b5c09434082cbcd5c
rakesh284619 <Reniguntla.RakeshRakesh@ibm.com>
2025-10-12 09:52:17 +0530

    Update Activity1feature.java

--- Cucumber/Activities/Activity1feature.java
+++ Cucumber/Activities/Activity1feature.java
@@ -1,5 +1,7 @@
-package Cucumber;
+@activity1
+Feature: Basic Syntax
 
-public class Activity1feature {
-
-}
+Scenario: Test the TS Home page
+  Given the user is on the TS home page
+  When they click on About Us page
+  Then they are redirected to About US page
