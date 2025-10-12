c5843f7f78eee7de9f54f5be97f8e781d773533d
rakesh284619 <Reniguntla.RakeshRakesh@ibm.com>
2025-10-12 10:02:21 +0530

    Delete Cucumber/Activities directory

--- Cucumber/Activities/Activity1feature.java
+++
@@ -1,7 +0,0 @@
-@activity1
-Feature: Basic Syntax
-
-Scenario: Test the TS Home page
-  Given the user is on the TS home page
-  When they click on About Us page
-  Then they are redirected to About US page
