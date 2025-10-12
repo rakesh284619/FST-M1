4ebb894b5fc3eec090ff3b0f550f9a2f691093e2
rakesh284619 <Reniguntla.RakeshRakesh@ibm.com>
2025-10-12 09:53:45 +0530

    Update Activity4feature.java

--- Cucumber/Activities/Activity4feature.java
+++ Cucumber/Activities/Activity4feature.java
@@ -1,5 +1,9 @@
-package Cucumber;
 
-public class Activity4feature {
+@activity4
+Feature: Data driven test without Examples
 
-}
+  Scenario: Testing with correct data from inputs
+    Given the user is on the login page
+    When the user enters "admin" and "password"
+    And clicks the submit button
+    Then get the confirmation text and verify message as "Welcome Back, Admin!"
