abcc4eb1e5944322f95757b1d2eb373e60bcd850
rakesh284619 <Reniguntla.RakeshRakesh@ibm.com>
2025-10-12 09:54:09 +0530

    Update Activity5feature.java

--- Cucumber/Activities/Activity5feature.java
+++ Cucumber/Activities/Activity5feature.java
@@ -1,5 +1,14 @@
-package Cucumber;
 
-public class Activity5feature {
+@activity5
+Feature: Data driven test with Examples
 
-}
+  Scenario Outline: Testing with Data from Scenario
+    Given the user is on the login page
+    When the user enters "<Usernames>" and "<Passwords>"
+    And clicks the submit button
+    Then get the confirmation text and verify message as "Invalid credentials"
+
+    Examples: 
+      | Usernames | Passwords      |
+      | admin     | password123    |
+      | admin     | wrongPassword  |
